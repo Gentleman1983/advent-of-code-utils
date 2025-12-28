c797fe9bd9a8d86cee798ae9d4c2fcc0e7e212f1
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2025-12-28 18:47:52 +0100

    Update LeftRightDirection

--- aoc-utils/aoc-utils-kotlin/src/main/kotlin/de/havox_design/aoc/utils/kotlin/model/directions/LeftRightDirection.kt
+++ aoc-utils/aoc-utils-kotlin/src/main/kotlin/de/havox_design/aoc/utils/kotlin/model/directions/LeftRightDirection.kt
@@ -1,10 +1,13 @@
 package de.havox_design.aoc.utils.kotlin.model.directions
 
-enum class LeftRightDirection(private val symbol: Char) {
-    LEFT('L'),
-    RIGHT('R');
+enum class LeftRightDirection(private val symbol: Char, val direction: Int) {
+    LEFT('L', -1),
+    RIGHT('R', 1);
 
     companion object {
+        fun from(string: String): LeftRightDirection =
+            from(string[0])
+
         fun from(char: Char): LeftRightDirection {
             if (entries.map { dir -> dir.symbol }.contains(char)) {
                 return entries.first { dir -> dir.symbol == char }
