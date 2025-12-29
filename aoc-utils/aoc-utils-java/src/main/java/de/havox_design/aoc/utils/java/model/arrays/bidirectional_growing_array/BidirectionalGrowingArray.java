bf42d4b549b46bea4b8c8f0b6547f21e87ad0588
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2025-12-29 13:28:34 +0100

    Add some JavaDoc and tests

--- aoc-utils/aoc-utils-java/src/main/java/de/havox_design/aoc/utils/java/model/arrays/bidirectional_growing_array/BidirectionalGrowingArray.java
+++ aoc-utils/aoc-utils-java/src/main/java/de/havox_design/aoc/utils/java/model/arrays/bidirectional_growing_array/BidirectionalGrowingArray.java
@@ -4,22 +4,52 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 import java.util.function.IntFunction;
 
+/**
+ *
+ * @param <T> the data type
+ */
 public class BidirectionalGrowingArray<T> implements Iterable<T> {
+    /**
+     * The array producer function.
+     */
     private final IntFunction<T[]> arrayProducer;
 
+    /**
+     * The internal array.
+     */
     private T[] array;
+    /**
+     * The offset for the start index.
+     */
     private int offset;
 
+    /**
+     * Constructor without offset.
+     *
+     * @param arrayProducer the array producing function
+     */
     public BidirectionalGrowingArray(final IntFunction<T[]> arrayProducer) {
         this(arrayProducer, 0);
     }
 
+    /**
+     * Constructor having an index offset.
+     *
+     * @param arrayProducer the array producing function
+     * @param startIndex the index offset
+     */
     public BidirectionalGrowingArray(final IntFunction<T[]> arrayProducer, final int startIndex) {
         this.arrayProducer = arrayProducer;
         this.array = arrayProducer.apply(0);
         this.offset = startIndex;
     }
 
+    /**
+     * Checks the internal array to provide space for the given entries on the {@link BidirectionalGrowingArray}.
+     *
+     * @param neededInternalIndex the index to be used
+     * @return the needed internal index
+     */
     private int checkSize(final int neededInternalIndex) {
         final int delta = neededInternalIndex < 0 ? -neededInternalIndex : neededInternalIndex - array.length + 1;
 
@@ -43,12 +73,24 @@ public class BidirectionalGrowingArray<T> implements Iterable<T> {
         return neededInternalIndex;
     }
 
+    /**
+     * Collects an element form the array.
+     *
+     * @param index the index to fetch
+     * @return the data or NULL
+     */
     public T get(final int index) {
         final int internalIndex = mapIndex(index);
 
         return (internalIndex < 0 || internalIndex >= array.length) ? null : array[internalIndex];
     }
 
+    /**
+     * Returns the index range of the {@link BidirectionalGrowingArray}.
+     *
+     * @return the minimum and maximum index of the array, e.g. {23, 42}
+     * @throws ArrayIndexOutOfBoundsException if called on empty array
+     */
     public int[] getRange() throws ArrayIndexOutOfBoundsException {
         if (isEmpty()) {
             throw new ArrayIndexOutOfBoundsException();
@@ -57,22 +99,46 @@ public class BidirectionalGrowingArray<T> implements Iterable<T> {
         return new int[] { offset, offset + array.length - 1 };
     }
 
-    private boolean isEmpty() {
+    /**
+     * Checks if the array is empty.
+     *
+     * @return true, if empty
+     */
+    public boolean isEmpty() {
         return size() == 0;
     }
 
+    /**
+     * Returns the {@link BidirectionalGrowingArray} iterator.
+     *
+     * @return the iterator
+     */
     @Override
     public Iterator<T> iterator() {
         return new Iterator<T>() {
+            /**
+             * The position on the array.
+             */
             int position;
 
+            /**
+             * Checks if there are more elements on the array.
+             *
+             * @return true, if there are additional elements.
+             */
             @Override
             public boolean hasNext() {
                 return position < array.length;
             }
 
+            /**
+             * Returns the next element on the array.
+             *
+             * @return the next element
+             * @throws NoSuchElementException if no next element is available
+             */
             @Override
-            public T next() {
+            public T next() throws NoSuchElementException {
                 if (hasNext()) {
                     return array[position++];
                 } else {
@@ -82,10 +148,23 @@ public class BidirectionalGrowingArray<T> implements Iterable<T> {
         };
     }
 
+    /**
+     * Maps between the given index using the offset and the internal array index.
+     *
+     * @param index the index using the offset.
+     * @return the internal index
+     */
     private int mapIndex(final int index) {
         return index - offset;
     }
 
+    /**
+     * Adds an entry to the {@link BidirectionalGrowingArray}.
+     *
+     * @param index the index the entry has to be put to. If already existing the entry will be updated.
+     * @param newValue the new value
+     * @return the old value
+     */
     public T put(final int index, final T newValue) {
         int internalIndex = mapIndex(index);
 
@@ -98,6 +177,11 @@ public class BidirectionalGrowingArray<T> implements Iterable<T> {
         return result;
     }
 
+    /**
+     * Checks the size of the array.
+     *
+     * @return the size
+     */
     public int size() {
         return array.length;
     }
--- aoc-utils/aoc-utils-java/src/test/java/de/havox_design/aoc/utils/java/model/arrays/bidirectional_growing_array/BidirectionalGrowingArrayTest.java
+++ aoc-utils/aoc-utils-java/src/test/java/de/havox_design/aoc/utils/java/model/arrays/bidirectional_growing_array/BidirectionalGrowingArrayTest.java
@@ -2,14 +2,216 @@ package de.havox_design.aoc.utils.java.model.arrays.bidirectional_growing_array;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.IntFunction;
 
 /**
  * Test class for {@link BidirectionalGrowingArray}.
  */
-
 class BidirectionalGrowingArrayTest {
+    /**
+     * Simple test for constructor {@link BidirectionalGrowingArray#BidirectionalGrowingArray(IntFunction)}.
+     */
     @Test
-    void testBasic() {
-        Assertions.assertTrue(true);
+    void testConstructorIntSource() {
+        IntFunction<Number[]> numberArrayProducer = Number[]::new;
+        IntFunction<Number[]> doubleArrayProducer = Double[]::new;
+        IntFunction<Number[]> floatArrayProducer = Float[]::new;
+        IntFunction<Number[]> longArrayProducer = Long[]::new;
+        IntFunction<Number[]> integerArrayProducer = Integer[]::new;
+
+        Assertions.assertAll(
+                () -> List
+                        .of(numberArrayProducer,
+                                doubleArrayProducer,
+                                floatArrayProducer,
+                                longArrayProducer,
+                                integerArrayProducer)
+                        .forEach(arrayProducer -> {
+                                    BidirectionalGrowingArray<Number> bidiArray = new BidirectionalGrowingArray<>(
+                                            arrayProducer
+                                    );
+                                    Assertions.assertAll(
+                                            () -> Assertions.assertNotNull(bidiArray),
+                                            () -> Assertions.assertInstanceOf(BidirectionalGrowingArray.class, bidiArray),
+                                            () -> Assertions.assertTrue(bidiArray.isEmpty()),
+                                            () -> Assertions.assertEquals(0, bidiArray.size())
+                                    );
+                                }
+                        )
+        );
+    }
+
+    /**
+     * Simple test for constructor {@link BidirectionalGrowingArray#BidirectionalGrowingArray(IntFunction, int)}.
+     *
+     * @param startIdex the start indices
+     */
+    @ParameterizedTest
+    @ValueSource(ints = {0, 5, 42, 123, -23})
+    void testConstructorIntSourceInteger(Integer startIdex) {
+        IntFunction<Number[]> numberArrayProducer = Number[]::new;
+        IntFunction<Number[]> doubleArrayProducer = Double[]::new;
+        IntFunction<Number[]> floatArrayProducer = Float[]::new;
+        IntFunction<Number[]> longArrayProducer = Long[]::new;
+        IntFunction<Number[]> integerArrayProducer = Integer[]::new;
+
+        Assertions.assertAll(
+                () -> List
+                        .of(numberArrayProducer,
+                                doubleArrayProducer,
+                                floatArrayProducer,
+                                longArrayProducer,
+                                integerArrayProducer)
+                        .forEach(arrayProducer -> {
+                                    BidirectionalGrowingArray<Number> bidiArray = new BidirectionalGrowingArray<>(
+                                            arrayProducer,
+                                            startIdex
+                                    );
+                                    Assertions.assertAll(
+                                            () -> Assertions.assertNotNull(bidiArray),
+                                            () -> Assertions.assertInstanceOf(BidirectionalGrowingArray.class, bidiArray),
+                                            () -> Assertions.assertTrue(bidiArray.isEmpty()),
+                                            () -> Assertions.assertEquals(0, bidiArray.size())
+                                    );
+                                }
+                        )
+        );
+    }
+
+    /**
+     * Simple test checking basic data manipulation functions.
+     */
+    @Test
+    void testDataManipulation() {
+        List<Integer> values = List.of(0, 37, 125, 42, 23, -17, 13, 7, 666, 0);
+        List<Integer> indices = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        int offset = 42;
+        IntFunction<Integer[]> arrayProducer = Integer[]::new;
+        BidirectionalGrowingArray<Integer> bidiArray = new BidirectionalGrowingArray<>(arrayProducer);
+        BidirectionalGrowingArray<Integer> bidiArrayOffset = new BidirectionalGrowingArray<>(arrayProducer, offset);
+
+        Assertions.assertAll(
+                () -> indices.forEach(index -> {
+                            bidiArray.put(index, values.get(index));
+                            Assertions.assertAll(
+                                    () -> Assertions.assertEquals(index + 1, bidiArray.size()),
+                                    () -> Assertions.assertEquals(values.get(index), bidiArray.get(index)),
+                                    () -> Assertions.assertArrayEquals(new int[]{0, index}, bidiArray.getRange())
+                            );
+
+                            bidiArrayOffset.put(index + offset, values.get(index));
+                            Assertions.assertAll(
+                                    () -> Assertions.assertEquals(index + 1, bidiArrayOffset.size()),
+                                    () -> Assertions.assertEquals(values.get(index), bidiArrayOffset.get(index + offset)),
+                                    () -> Assertions.assertArrayEquals(
+                                            new int[]{offset, index + offset},
+                                            bidiArrayOffset.getRange()
+                                    )
+                            );
+                        }
+                )
+        );
+    }
+
+    /**
+     * Test checking empty element checks on methods.
+     *
+     * @param withOffset test instance with or without offset
+     */
+    @ParameterizedTest
+    @ValueSource(booleans = {
+            true,
+            false
+    })
+    void testGetRangeWithEmptyArray(boolean withOffset) {
+        int offset = 42;
+        IntFunction<Integer[]> arrayProducer = Integer[]::new;
+        BidirectionalGrowingArray<Integer> bidiArray = withOffset ?
+                new BidirectionalGrowingArray<>(arrayProducer, offset) :
+                new BidirectionalGrowingArray<>(arrayProducer);
+
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(bidiArray.isEmpty()),
+                () -> Assertions.assertThrows(ArrayIndexOutOfBoundsException.class, bidiArray::getRange)
+        );
+    }
+
+    /**
+     * Checks if NULL is returned on non-existing entries.
+     *
+     * @param withOffset test instance with or without offset
+     */
+    @ParameterizedTest
+    @ValueSource(booleans = {
+            true,
+            false
+    })
+    void testGetFromNotExistingEntries(boolean withOffset) {
+        int offset = 42;
+        IntFunction<Integer[]> arrayProducer = Integer[]::new;
+        BidirectionalGrowingArray<Integer> bidiArray = withOffset ?
+                new BidirectionalGrowingArray<>(arrayProducer, offset) :
+                new BidirectionalGrowingArray<>(arrayProducer);
+
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(bidiArray.isEmpty()),
+                () -> Assertions.assertNull(bidiArray.get(23))
+        );
+    }
+
+    /**
+     * Basic test testing the functionality of the {@link BidirectionalGrowingArray#iterator()}.
+     */
+    @Test
+    void testIterator() {
+        List<Integer> values = List.of(0, 37, 125, 42, 23, -17, 13, 7, 666, 0);
+        List<Integer> indices = List.of(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        int offset = 42;
+        IntFunction<Integer[]> arrayProducer = Integer[]::new;
+        BidirectionalGrowingArray<Integer> bidiArray = new BidirectionalGrowingArray<>(arrayProducer);
+        BidirectionalGrowingArray<Integer> bidiArrayOffset = new BidirectionalGrowingArray<>(arrayProducer, offset);
+
+        indices.forEach(index -> {
+            bidiArray.put(index, values.get(index));
+            bidiArrayOffset.put(index + offset, values.get(index));
+        });
+        Iterator<Integer> bidiArrayIterator = bidiArray.iterator();
+        Iterator<Integer> bidiArrayOffsetIterator = bidiArrayOffset.iterator();
+
+
+        Assertions.assertAll(
+                () -> Assertions.assertNotNull(bidiArrayIterator),
+                () -> Assertions.assertNotNull(bidiArrayOffsetIterator),
+                () -> {
+                    for(int i=0; i <= 9; i++) {
+                        Assertions.assertAll(
+                                () -> Assertions.assertTrue(bidiArrayIterator.hasNext()),
+                                () -> Assertions.assertTrue(bidiArrayOffsetIterator.hasNext())
+                        );
+
+                        int finalI = i;
+                        int value = bidiArrayIterator.next();
+                        int valueOffset = bidiArrayOffsetIterator.next();
+
+                        Assertions.assertAll(
+                                () -> Assertions.assertEquals(value, values.get(finalI)),
+                                () -> Assertions.assertEquals(valueOffset, values.get(finalI))
+                        );
+                    }
+
+                    Assertions.assertAll(
+                            () -> Assertions.assertFalse(bidiArrayIterator.hasNext()),
+                            () -> Assertions.assertThrows(NoSuchElementException.class, bidiArrayIterator::next),
+                            () -> Assertions.assertFalse(bidiArrayOffsetIterator.hasNext()),
+                            () -> Assertions.assertThrows(NoSuchElementException.class, bidiArrayOffsetIterator::next)
+                    );
+                }
+        );
     }
 }
