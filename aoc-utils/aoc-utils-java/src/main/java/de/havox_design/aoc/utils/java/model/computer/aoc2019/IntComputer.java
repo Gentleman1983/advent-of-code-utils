2a710c19352d08a3ce30ee88e351a82380cdcc57
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2025-12-29 20:11:01 +0100

    Add some JavaDoc and tests

--- aoc-utils/aoc-utils-java/src/main/java/de/havox_design/aoc/utils/java/model/computer/aoc2019/IntComputer.java
+++ aoc-utils/aoc-utils-java/src/main/java/de/havox_design/aoc/utils/java/model/computer/aoc2019/IntComputer.java
@@ -6,10 +6,22 @@ import java.util.*;
 import java.util.concurrent.*;
 import java.util.logging.Logger;
 
+/**
+ * The IntComputer used widely during Advent of Code 2019.
+ */
 @SuppressWarnings("javaarchitecture:S7027")
 public class IntComputer implements Runnable {
+    /**
+     * The executor.
+     */
     private static final ExecutorService EXECUTOR = Executors.newCachedThreadPool();
+    /**
+     * The logger.
+     */
     private static final Logger LOGGER = Logger.getLogger(IntComputer.class.getName());
+    /**
+     * All Opcode data elements.
+     */
     private static final Set<OpCodeData> OPCODES = Set.of(
             new OpCodeData("ADD", 1, 3),
             new OpCodeData("MULTIPLY", 2, 3),
@@ -23,23 +35,64 @@ public class IntComputer implements Runnable {
             new OpCodeData("HALT", 99, 0),
             new OpCodeData("NOP", -1, 0)
     );
+    /**
+     * Stop printing.
+     */
     private static final boolean PRINT_HALT = false;
 
+    /**
+     * The input queue.
+     */
     private final BlockingQueue<Long> in;
+    /**
+     * The computer memory.
+     */
     private Map<Long, Long> memory;
+    /**
+     * The processing mode.
+     */
     private final boolean modes;
+    /**
+     * The current pointer.
+     */
     private long pointer = 0L;
+    /**
+     * The output queue.
+     */
     private final BlockingQueue<Long> out;
+    /**
+     *the relative base.
+     */
     private long relativeBase = 0L;
 
+    /**
+     * The constructor.
+     *
+     * @param program the program to process
+     */
     public IntComputer(final List<Long> program) {
         this(false, program, null, null);
     }
 
+    /**
+     * The constructor using input and output queues.
+     *
+     * @param program the program to process
+     * @param in the input queue
+     * @param out the output queue
+     */
     public IntComputer(final List<Long> program, final BlockingQueue<Long> in, final BlockingQueue<Long> out) {
         this(true, program, in, out);
     }
 
+    /**
+     * The constructor using input and output queues and modes.
+     *
+     * @param modes the processing mode
+     * @param program the program to process
+     * @param in the input queue
+     * @param out the output queue
+     */
     private IntComputer(final boolean modes, final List<Long> program, final BlockingQueue<Long> in, final BlockingQueue<Long> out) {
         this.modes = modes;
         this.in = in;
@@ -47,14 +100,29 @@ public class IntComputer implements Runnable {
         loadProgram(program);
     }
 
+    /**
+     * Advances the pointer to the next instruction.
+     *
+     * @return the next instruction
+     */
     public Long advancePointer() {
         return ++pointer;
     }
 
+    /**
+     * Checks the id of the current instruction.
+     *
+     * @return the current instruction
+     */
     protected Long currentInstruction() {
         return getValueDirect(pointer);
     }
 
+    /**
+     * Executes one program step.
+     *
+     * @return is the program finished
+     */
     public boolean executeOneStep() {
         final Optional<OpCode> opCode = getCurrentOpCode();
 
@@ -64,6 +132,11 @@ public class IntComputer implements Runnable {
         return opCode.orElse(HALT) != HALT;
     }
 
+    /**
+     * Loads a given program
+     *
+     * @param program the program
+     */
     private void loadProgram(final List<Long> program) {
         memory = new HashMap<>();
 
@@ -72,6 +145,11 @@ public class IntComputer implements Runnable {
         }
     }
 
+    /**
+     * Reads the current {@link OpCode}.
+     *
+     * @return the {@link OpCode}
+     */
     private Optional<OpCode> getCurrentOpCode() {
         long opCode = currentInstruction();
 
@@ -82,6 +160,12 @@ public class IntComputer implements Runnable {
         return Optional.ofNullable(OpCode.valueOf(opCode));
     }
 
+    /**
+     * Reads the data for a given {@link OpCode} name.
+     *
+     * @param opcodeName the name
+     * @return the data
+     */
     public static OpCodeData getDataFor(String opcodeName) {
         return OPCODES
                 .stream()
@@ -90,14 +174,31 @@ public class IntComputer implements Runnable {
                 .orElseThrow();
     }
 
+    /**
+     * Gets the input queue.
+     *
+     * @return the input queue
+     */
     protected BlockingQueue<Long> getIn() {
         return in;
     }
 
+    /**
+     * Gets the memory.
+     *
+     * @return the memory
+     */
     public Map<Long, Long> getMemory() {
         return memory;
     }
 
+    /**
+     * Returns the mode.
+     *
+     * @param value the value
+     * @param param the parameter
+     * @return the mode
+     */
     protected int getMode(final long value, final int param) {
         int position = 10;
 
@@ -108,6 +209,12 @@ public class IntComputer implements Runnable {
         return (int) ((value / position) % 10);
     }
 
+    /**
+     * Returns the next parameter.
+     *
+     * @param mode the mode
+     * @return the next parameter
+     */
     protected Long getNextParameter(final int mode) {
         if (mode == 1) {
             return getValueDirect(advancePointer());
@@ -116,32 +223,70 @@ public class IntComputer implements Runnable {
         }
     }
 
+    /**
+     * Returns the output queue.
+     *
+     * @return the output queue
+     */
     protected BlockingQueue<Long> getOut() {
         return out;
     }
 
+    /**
+     * Returns the pointer.
+     *
+     * @return the pointer
+     */
     protected long getPointer() {
         return pointer;
     }
 
+    /**
+     * Returns the print halt parameter.
+     *
+     * @return the parameter
+     */
     public static boolean getPrintHalt() {
         return PRINT_HALT;
     }
 
+    /**
+     * Returns the relative base.
+     *
+     * @return the relative base
+     */
     protected long getRelativeBase() {
         return relativeBase;
     }
 
+    /**
+     * Returns a value from a memory address.
+     *
+     * @param address the address
+     * @return the value or 0 if no value is available at the address
+     */
     private Long getValueDirect(final Long address) {
         return memory.getOrDefault(address, 0L);
     }
 
+    /**
+     * Returns the value indirectly.
+     *
+     * @param index the index
+     * @param relative use relative address
+     * @return the value
+     */
     private Long getValueIndirect(final Long index, final boolean relative) {
         final long offSet = relative ? relativeBase : 0;
 
         return getValueDirect(getValueDirect(index) + offSet);
     }
 
+    /**
+     * Prints a processing step.
+     *
+     * @return the {@link OpCode}
+     */
     public Optional<OpCode> printOneStep() {
         final Optional<OpCode> opCode = getCurrentOpCode();
 
@@ -150,6 +295,13 @@ public class IntComputer implements Runnable {
         return opCode;
     }
 
+    /**
+     * Prints the address of the parameter.
+     *
+     * @param mode the mode
+     * @param param the parameter
+     * @return the address
+     */
     protected String printParameter(final int mode, final int param) {
         final Long address = getValueDirect(pointer + param);
 
@@ -160,6 +312,9 @@ public class IntComputer implements Runnable {
         }
     }
 
+    /**
+     * Prints the complete program run.
+     */
     public void printProgram() {
         LOGGER.info("=======BEGIN PROGRAM=======");
         final long origPointer = pointer;
@@ -176,11 +331,17 @@ public class IntComputer implements Runnable {
         this.pointer = origPointer;
     }
 
+    /**
+     * Resets the {@link IntComputer}.
+     */
     public void reset() {
         this.pointer = 0L;
         this.relativeBase = 0L;
     }
 
+    /**
+     * Start synchronous processing.
+     */
     @Override
     public void run() {
         while (true) {
@@ -190,11 +351,25 @@ public class IntComputer implements Runnable {
         }
     }
 
+    /**
+     * Starts asynchronous processing.
+     *
+     * @return the execution
+     */
     @SuppressWarnings("squid:S1452")
     public Future<?> runAsync() {
         return EXECUTOR.submit(this);
     }
 
+    /**
+     * Runs the whole computer.
+     *
+     * @param program the program
+     * @param in the input queue
+     * @param out the output queue
+     * @param async process asynchronous?
+     * @return the execution
+     */
     @SuppressWarnings("squid:S1452")
     public static Future<?> runComputer(final List<Long> program, final BlockingQueue<Long> in, final BlockingQueue<Long> out, final boolean async) {
         final IntComputer computer = new IntComputer(program, in, out);
@@ -210,34 +385,74 @@ public class IntComputer implements Runnable {
         return future;
     }
 
+    /**
+     * Sets the next parameter.
+     *
+     * @param val the value
+     * @param mode the mode
+     * @return the old value
+     */
     protected Long setNextParameter(final Long val, final int mode) {
         return setValueIndirect(advancePointer(), val, mode == 2);
     }
 
+    /**
+     * Sets the pointer position.
+     *
+     * @param value the position
+     */
     protected void setPointer(long value) {
         pointer = value;
     }
 
+    /**
+     * Sets the pointer position.
+     *
+     * @param pointer the position
+     */
     private void setPointer(final int pointer) {
         this.pointer = pointer;
     }
 
+    /**
+     * Sets the relative base.
+     *
+     * @param value the base
+     */
     protected void setRelativeBase(long value) {
         relativeBase = value;
     }
 
+    /**
+     * Sets the value directly.
+     *
+     * @param address the address
+     * @param val the value
+     * @return the old value
+     */
     private Long setValueDirect(final Long address, final Long val) {
         Objects.requireNonNull(val);
 
         return memory.put(address, val);
     }
 
+    /**
+     * Sets the value indirectly.
+     *
+     * @param index the index
+     * @param val the value
+     * @param relative the relative flag
+     * @return the old value
+     */
     private Long setValueIndirect(final Long index, final Long val, final boolean relative) {
         final long offSet = relative ? relativeBase : 0;
 
         return setValueDirect(getValueDirect(index) + offSet, val);
     }
 
+    /**
+     * Shuts down the executor.
+     */
     public static void shutdownAll() {
         EXECUTOR.shutdown();
     }
