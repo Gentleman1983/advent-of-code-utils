62f85790846bd1dbd4b33bb0247ec69a9131297e
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2026-04-16 19:48:41 +0200

    Added BooleanHelperTest

--- aoc-utils/aoc-utils-kotlin/src/test/kotlin/de/havox_design/aoc/utils/kotlin/helpers/BooleanHelperTest.kt
+++ aoc-utils/aoc-utils-kotlin/src/test/kotlin/de/havox_design/aoc/utils/kotlin/helpers/BooleanHelperTest.kt
@@ -1,16 +1,27 @@
 package de.havox_design.aoc.utils.kotlin.helpers
 
-import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.Test
+import de.havox_design.aoc.utils.kotlin.helpers.math.ExtendedGcd
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
 
-/**
- * Test for BooleanHelper class.
- *
- * @constructor Creates test
- */
+
 class BooleanHelperTest {
-    @Test
-    fun testBasic() {
-        Assertions.assertTrue(true)
+    @ParameterizedTest
+    @MethodSource("getDataForToIntTests")
+    fun testToInt(objectUnderTest: Boolean, expectedValue: Int) {
+        assertEquals(objectUnderTest.toInt(), expectedValue)
+    }
+
+    companion object{
+        @JvmStatic
+        private fun getDataForToIntTests(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of(true, 1),
+                Arguments.of(false, 0)
+            )
+
     }
 }
