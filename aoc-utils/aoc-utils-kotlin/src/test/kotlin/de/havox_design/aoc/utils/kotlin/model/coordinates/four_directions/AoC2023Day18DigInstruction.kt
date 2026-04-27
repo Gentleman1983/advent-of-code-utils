7297bb2e43ceb57331e3c50198117d68aaf66487
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2026-04-27 23:40:18 +0200

    Add tests for FourDirections coordinate

---
+++ aoc-utils/aoc-utils-kotlin/src/test/kotlin/de/havox_design/aoc/utils/kotlin/model/coordinates/four_directions/AoC2023Day18DigInstruction.kt
@@ -0,0 +1,16 @@
+package de.havox_design.aoc.utils.kotlin.model.coordinates.four_directions
+
+data class AoC2023Day18DigInstruction(val direction: FourDirectionsFlipped, val amount: Int, val colorCode: String) {
+    fun transformInstruction(): AoC2023Day18DigInstruction {
+        val steps = colorCode.drop(1).take(5).toInt(16)
+        val direction = when (colorCode.last()) {
+            '0' -> FourDirectionsFlipped.RIGHT
+            '1' -> FourDirectionsFlipped.DOWN
+            '2' -> FourDirectionsFlipped.LEFT
+            '3' -> FourDirectionsFlipped.UP
+            else -> error("invalid $colorCode")
+        }
+
+        return copy(direction = direction, amount = steps)
+    }
+}
