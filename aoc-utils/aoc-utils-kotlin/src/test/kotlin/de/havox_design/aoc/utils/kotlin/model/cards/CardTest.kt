c186a698aa3dc070ac055d8c60e21c0c84858d5d
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2026-04-27 22:12:22 +0200

    Added tests for Card

--- aoc-utils/aoc-utils-kotlin/src/test/kotlin/de/havox_design/aoc/utils/kotlin/model/cards/CardTest.kt
+++ aoc-utils/aoc-utils-kotlin/src/test/kotlin/de/havox_design/aoc/utils/kotlin/model/cards/CardTest.kt
@@ -1,16 +1,19 @@
 package de.havox_design.aoc.utils.kotlin.model.cards
 
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 /**
  * Test for Card class.
- *
- * @constructor Creates test
  */
 class CardTest {
     @Test
-    fun testBasic() {
-        Assertions.assertTrue(true)
+    fun testThrowsExceptionOnUnknownSymbol() {
+        val unknownSymbol: Char = 'x'
+        val expectedMessage = "No knows card symbol '${unknownSymbol}'"
+
+        val exception = assertThrows(IllegalArgumentException::class.java) { Card.from(unknownSymbol) }
+        assertEquals(expectedMessage, exception.message)
     }
 }
