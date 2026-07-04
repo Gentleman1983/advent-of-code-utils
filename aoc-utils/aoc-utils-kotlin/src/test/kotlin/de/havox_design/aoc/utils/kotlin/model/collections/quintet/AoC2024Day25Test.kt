f8dc6813d5e6ab8bcb3294c89d77508aa947782e
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2026-07-04 11:19:41 +0200

    Add tests for Quintet

---
+++ aoc-utils/aoc-utils-kotlin/src/test/kotlin/de/havox_design/aoc/utils/kotlin/model/collections/quintet/AoC2024Day25Test.kt
@@ -0,0 +1,23 @@
+package de.havox_design.aoc.utils.kotlin.model.collections.quintet
+
+import de.havox_design.aoc.utils.kotlin.helpers.tests.shouldBe
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import java.util.stream.Stream
+
+class AoC2024Day25Test {
+
+    @ParameterizedTest
+    @MethodSource("getDataForTestProcessPart1")
+    fun testProcessPart1(filename: String, expectedResult: Int) =
+        AoC2024Day25CodeChronicle(filename).processPart1().shouldBe(expectedResult)
+
+    companion object {
+        @JvmStatic
+        private fun getDataForTestProcessPart1(): Stream<Arguments> =
+            Stream.of(
+                Arguments.of("de/havox_design/aoc2024/day25/day25sample.txt", 3)
+            )
+    }
+}
