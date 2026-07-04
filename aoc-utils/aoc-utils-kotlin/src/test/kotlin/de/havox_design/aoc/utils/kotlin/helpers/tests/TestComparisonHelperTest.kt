3b94457f1f4fb9eb273007b14a0808662b7831e3
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2026-07-04 11:40:35 +0200

    Add tests for TestComparisonHelper

--- aoc-utils/aoc-utils-kotlin/src/test/kotlin/de/havox_design/aoc/utils/kotlin/helpers/tests/TestComparisonHelperTest.kt
+++ aoc-utils/aoc-utils-kotlin/src/test/kotlin/de/havox_design/aoc/utils/kotlin/helpers/tests/TestComparisonHelperTest.kt
@@ -2,6 +2,7 @@ package de.havox_design.aoc.utils.kotlin.helpers.tests
 
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
+import java.math.BigInteger
 
 /**
  * Test for TestComparisonHelper class.
@@ -10,7 +11,42 @@ import org.junit.jupiter.api.Test
  */
 class TestComparisonHelperTest {
     @Test
-    fun testBasic() {
-        Assertions.assertTrue(true)
+    fun testShouldBeBigInter() {
+        val objectUnderTest = BigInteger.valueOf(42);
+        val expectedValue = BigInteger.valueOf(42);
+
+        objectUnderTest.shouldBe(expectedValue)
+    }
+
+    @Test
+    fun testShouldBeString() {
+        val objectUnderTest = "Hello World";
+        val expectedValue = "Hello World";
+
+        objectUnderTest.shouldBe(expectedValue)
+    }
+
+    @Test
+    fun testShouldBeMap() {
+        val objectUnderTest = mapOf("Hello" to "World", "first" to "here", "last" to "here");
+        val expectedValue = mapOf("Hello" to "World", "last" to "here", "first" to "here");
+
+        objectUnderTest.shouldBe(expectedValue)
+    }
+
+    @Test
+    fun testShouldContainAllCollection() {
+        val objectUnderTest = listOf("Hello", "here", "there", "World").toCollection(ArrayList<String>());
+        val expectedValue = listOf("Hello", "World").toCollection(ArrayList<String>());
+
+        objectUnderTest.shouldContainAll(expectedValue)
+    }
+
+    @Test
+    fun testShouldContainAllCollectionMessage() {
+        val objectUnderTest = listOf("Hello", "here", "there", "World").toCollection(ArrayList<String>());
+        val expectedValue = listOf("Hello", "World").toCollection(ArrayList<String>());
+
+        objectUnderTest.shouldContainAll(expectedValue, "foobar")
     }
 }
