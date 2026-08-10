255102de3cdb383fd0f32bc2dc089b0b14458f67
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2026-08-10 22:00:33 +0200

    Revert

--- aoc-utils/aoc-utils-java/build.gradle.kts
+++ aoc-utils/aoc-utils-java/build.gradle.kts
@@ -8,10 +8,3 @@ dependencies {
     testImplementation(libs.commons.collections4)
     testImplementation(libs.commons.codec)
 }
-
-val sonarResolverTask = tasks.sonarResolver.get()
-val kotlinProject = project.parent?.subprojects?.filter { proj -> proj.name.equals("aoc-utils-kotlin") }[0]
-gradle.taskGraph.whenReady {
-    sonarResolverTask.inputs.files(kotlinProject?.tasks?.getByName("compileKotlin"))
-    sonarResolverTask.inputs.files(kotlinProject?.tasks?.getByName("compileJava"))
-}
