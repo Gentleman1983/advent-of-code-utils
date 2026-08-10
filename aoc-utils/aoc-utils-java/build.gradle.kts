1baec17f023b9a80d79a6e20544592329926dca8
The_Gentleman83 (Christian Otto) <Christian@Otto-Uslar.de>
2026-08-10 21:43:03 +0200

    Added implicit inputdependency for sonarResolver task of aoc-utils-jave project

--- aoc-utils/aoc-utils-java/build.gradle.kts
+++ aoc-utils/aoc-utils-java/build.gradle.kts
@@ -1,3 +1,5 @@
+import org.gradle.kotlin.dsl.sonarResolver
+
 dependencies {
     implementation(utilityLibs.jakarta.annotation.api)
     implementation(utilityLibs.struts2.core)
@@ -5,4 +7,11 @@ dependencies {
     testImplementation(project(":aoc-utils:aoc-utils-kotlin"))
     testImplementation(libs.commons.collections4)
     testImplementation(libs.commons.codec)
+}
+
+val sonarResolverTask = tasks.sonarResolver.get()
+val kotlinProject = project.parent?.subprojects?.filter { proj -> proj.name.equals("aoc-utils-kotlin") }[0]
+gradle.taskGraph.whenReady {
+    sonarResolverTask.inputs.files(kotlinProject?.tasks?.getByName("compileKotlin"))
+    sonarResolverTask.inputs.files(kotlinProject?.tasks?.getByName("compileJava"))
 }
